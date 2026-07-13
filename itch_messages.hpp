a60221fd5dabae60b25f604977e9cbbf13e8e996
vedantshirude05@gmail.com <vss24@ic.ac.uk>
2026-07-13 21:41:19 +0100

    feat: add itch message structures

---
+++ itch_messages.hpp
@@ -0,0 +1,106 @@
+#ifndef ITCH_MESSAGES_HPP
+#define ITCH_MESSAGES_HPP
+
+#include <cstdint>
+
+enum class MsgType : char {
+    SystemEvent    = 'S',
+    StockDirectory = 'R',
+    AddOrder       = 'A',
+    DeleteOrder    = 'D',
+    ExecuteOrder   = 'E',
+    CancelOrder = 'X',
+    ReplaceOrder = 'U'
+};
+
+// __attribute__((packed)) ensures struct is stored contiguosly in memory.
+struct __attribute__((packed)) SystemEventMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    char event_code;
+};
+
+struct __attribute__((packed)) StockDirectoryMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    char stock[8];
+    char market_category;
+    char financial_status_indicator;
+    uint32_t round_lot_size;
+    char round_lots_only;
+    char issue_classification;
+    char issue_subtype[2];
+    char authenticity;
+    char short_sale_threshold_indicator;
+    char ipo_flag;
+    char LULD_reference_price_tier;
+    char ETP_flag;
+    uint32_t ETP_leverage_factor;
+    char inverse_indicator;
+};
+
+struct __attribute__((packed)) AddOrderMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    uint64_t order_reference_number;
+    char buy_sell_indicator;
+    uint32_t shares;
+    char stock[8];
+    uint32_t price;
+};
+
+struct __attribute__((packed)) DeleteOrderMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    uint64_t order_reference_num;
+};
+
+struct __attribute__((packed)) ExecuteOrderMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    uint64_t order_reference_num;
+    uint32_t executed_shares;
+    uint64_t match_number;
+};
+
+struct __attribute__((packed)) CancelOrderMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    uint64_t order_reference_num;
+    uint32_t cancelled_shares;
+};
+
+struct __attribute__((packed)) ReplaceOrderMessage {
+    char msg_type;
+    uint16_t stock_locate;
+    uint16_t tracking_num;
+    uint8_t timestamp[6];
+    uint64_t original_order_reference_num;
+    uint64_t new_order_reference_num;
+    uint32_t shares;
+    uint32_t price;
+};
+
+// Convert timestamp to uint64_t
+inline uint64_t read_timestamp(const uint8_t ts[6]) {
+    return (uint64_t)ts[0] << 40 |
+           (uint64_t)ts[1] << 32 |
+           (uint64_t)ts[2] << 24 |
+           (uint64_t)ts[3] << 16 |
+           (uint64_t)ts[4] <<  8 |
+           (uint64_t)ts[5];
+}
+
+#endif
